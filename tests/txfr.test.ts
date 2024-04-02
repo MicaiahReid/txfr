4e146ad4183c0e1e181d286623f1582c212f4419
MicaiahReid <micaiahreid@gmail.com>
2024-04-02 10:10:52 -0400

    clean up test

--- tests/txfr.test.ts
+++ tests/txfr.test.ts
@@ -4,13 +4,8 @@ import { Cl } from "@stacks/transactions";
 const accounts = simnet.getAccounts();
 const address1 = accounts.get("wallet_1")!;
 
-/*
-  The test below is an example. To learn more, read the testing documentation here:
-  https://docs.hiro.so/clarinet/feature-guides/test-contract-with-clarinet-sdk
-*/
-
-describe("example tests", () => {
-  it("ensures simnet is well initalised", () => {
+describe("cost optimization", () => {
+  it("deploying a contract with hardcoded addresses", () => {
     const transfer = simnet.callPrivateFn(
       "txfr",
       "trans",
@@ -19,9 +14,4 @@ describe("example tests", () => {
     );
     console.log(transfer.result);
   });
-
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
 });
