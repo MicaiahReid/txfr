6fb6e394b40bb674dab465bc18bd779520a648cd
MicaiahReid <micaiahreid@gmail.com>
2024-04-02 10:10:22 -0400

    first commit

---
+++ tests/txfr.test.ts
@@ -0,0 +1,27 @@
+import { describe, expect, it } from "vitest";
+import { Cl } from "@stacks/transactions";
+
+const accounts = simnet.getAccounts();
+const address1 = accounts.get("wallet_1")!;
+
+/*
+  The test below is an example. To learn more, read the testing documentation here:
+  https://docs.hiro.so/clarinet/feature-guides/test-contract-with-clarinet-sdk
+*/
+
+describe("example tests", () => {
+  it("ensures simnet is well initalised", () => {
+    const transfer = simnet.callPrivateFn(
+      "txfr",
+      "trans",
+      [Cl.principal(address1)],
+      address1
+    );
+    console.log(transfer.result);
+  });
+
+  // it("shows an example", () => {
+  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
+  //   expect(result).toBeUint(0);
+  // });
+});
